5dbc0793c7538712529d016617d5bb84edfc614a
dhmec <dharmoradiya@gmail.com>
2020-11-10 12:08:03 -0700

    Converted  Class Based FetchData.js to Functional;  Converted the fetch to an Axios call

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -1,61 +1,67 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export class FetchData extends Component {
-  static displayName = FetchData.name;
+function FetchData(props) {
+    const displayName = FetchData.name;
 
-  constructor(props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
-  }
+    // Configure our state, and our setState standin methods.
+    const [forecasts, setForecasts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
-    this.populateWeatherData();
-  }
+    // Build the table based on forecast data.
+    function renderForecastsTable(forecasts) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Temp. (C)</th>
+                        <th>Temp. (F)</th>
+                        <th>Precipitation</th>
+                        <th>Summary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {forecasts.map(forecast =>
+                        <tr key={forecast.date}>
+                            <td>{forecast.date}</td>
+                            <td>{forecast.temperatureC}</td>
+                            <td>{forecast.temperatureF}</td>
+                            <td>{forecast.precipitation}</td>
+                            <td>{forecast.summary}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
 
-  static renderForecastsTable(forecasts) {
+    // Grab our data from our API.
+    async function populateWeatherData() {
+        // npm install --save axios
+        const response = await axios.get('weatherforecast');
+        setForecasts(response.data);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        populateWeatherData();
+    }, [loading]);
+
+    let contents = loading
+        ? <p><em>Loading...</em></p>
+        : renderForecastsTable(forecasts);
+
     return (
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-            <th>Precipitaion</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-              <td>{forecast.precipitation}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+        <div>
+            <h1 id="tabelLabel" >Weather forecast</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+            {contents}
+
+            <button className="btn btn-primary" onClick={() => { setLoading(true) }}>Refresh</button>
+        </div>
     );
-  }
-
-  render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
-
-    return (
-      <div>
-        <h1 id="tabelLabel" >Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
-        {contents}
-      </div>
-    );
-  }
-
-  async populateWeatherData() {
-    const response = await fetch('weatherforecast');
-    const data = await response.json();
-    this.setState({ forecasts: data, loading: false });
-  }
 }
+
+export { FetchData };
+
